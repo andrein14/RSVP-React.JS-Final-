9b6ead387848662a7812d0816116ee68ee315589
AndreiN14 <andrei.nemes14@yahoo.com>
2019-01-23 17:39:01 +0200

    Updates

---
+++ precache-manifest.4d8baba0bbec3f8bef7a94c2265ba951.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "be92ab00f0f555419adb491aa392b9ca",
+    "url": "/RSVP-React.JS-Final-/static/media/header-bg.be92ab00.jpg"
+  },
+  {
+    "revision": "96f05328c5876ae01dfd",
+    "url": "/RSVP-React.JS-Final-/static/js/runtime~main.96f05328.js"
+  },
+  {
+    "revision": "e8302fccb2b0b4877833",
+    "url": "/RSVP-React.JS-Final-/static/js/main.e8302fcc.chunk.js"
+  },
+  {
+    "revision": "b7f75e48d2712b6c8270",
+    "url": "/RSVP-React.JS-Final-/static/js/1.b7f75e48.chunk.js"
+  },
+  {
+    "revision": "e8302fccb2b0b4877833",
+    "url": "/RSVP-React.JS-Final-/static/css/main.d7cfd977.chunk.css"
+  },
+  {
+    "revision": "fc01f018889bc52a0ecace5afbb121c7",
+    "url": "/RSVP-React.JS-Final-/index.html"
+  }
+];
